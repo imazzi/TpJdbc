9883d8a93d751f19cd7431192652ff03a2d68557
imazzi <asmae.imazzi@edu.uiz.ac.ma>
2022-03-08 15:58:38 +0100

    article-asmae

--- src/main/java/com/norsys/dao/impl/ClientDaoImpl.java
+++ src/main/java/com/norsys/dao/impl/ClientDaoImpl.java
@@ -2,7 +2,13 @@ package com.norsys.dao.impl;
 
 import com.norsys.models.DoClient;
 import com.norsys.dao.ClientDao;
+import com.norsys.util.DbConnection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientDaoImpl implements ClientDao {
